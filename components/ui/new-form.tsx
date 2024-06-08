fd3c901c6eda1147981b98c9b5420d61485b5c74
古垣　博光 <h.f.appmaker@gmail.com>
2024-06-08 08:49:03 +0000

    radix uiに変更 radix uiは見た目の情報がないらしい

--- components/ui/new-form.tsx
+++ components/ui/new-form.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react';
 import { useFormState } from 'react-dom';
 import { toast } from "sonner";
+import * as Form from '@radix-ui/react-form';
 
 interface State {
     error?: string | undefined,
@@ -12,7 +13,8 @@ const NewForm: React.FC<{
     action: (prevState: State, data: FormData) => Promise<State>,
     children: React.ReactNode | React.ReactNode[],
 }> = (props) => {
-    const [state, formDispatch] = useFormState(props.action, { error: undefined, success: undefined });
+    const { action, children } = props;
+    const [state, formDispatch] = useFormState(action, { error: undefined, success: undefined });
 
     useEffect(() => {
         if (state.error) {
@@ -25,9 +27,9 @@ const NewForm: React.FC<{
     }, [state]);
 
     return (
-        <form action={formDispatch}>
-            {props.children}
-        </form>
+        <Form.Root action={formDispatch}>
+            {children}
+        </Form.Root>
     );
 };
 
