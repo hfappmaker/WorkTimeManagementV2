df651fef9415234a2cf61881b034bc4c279dec26
古垣　博光 <h.f.appmaker@gmail.com>
2025-03-16 07:14:43 +0000

    Optimize chunking in Next.js configuration for improved performance
    - Added configuration for splitChunks in next.config.js to enhance code splitting and loading efficiency.
    - Set parameters for minSize, maxSize, and cacheGroups to optimize how chunks are created and reused, improving overall application performance.

--- next.config.js
+++ next.config.js
@@ -10,6 +10,32 @@ const nextConfig = {
           aggregateTimeout: 300
         }
       }
+      
+      // Optimize chunks
+      config.optimization = {
+        ...config.optimization,
+        splitChunks: {
+          chunks: 'all',
+          minSize: 20000,
+          maxSize: 244000,
+          minChunks: 1,
+          maxAsyncRequests: 30,
+          maxInitialRequests: 30,
+          cacheGroups: {
+            defaultVendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              reuseExistingChunk: true,
+            },
+            default: {
+              minChunks: 2,
+              priority: -20,
+              reuseExistingChunk: true,
+            },
+          },
+        },
+      }
+      
       return config
     },
   }
